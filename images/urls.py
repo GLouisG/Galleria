31834e38bd685623d8f36f733b82607c2288db44
GLouisG <louis.gacho@student.moringaschool.com>
2021-10-09 20:53:39 +0300

    add settings for image upload

--- images/urls.py
+++ images/urls.py
@@ -9,5 +9,5 @@ urlpatterns=[
     url(r'^location/?P<location>\w+/', views.by_location, name='location')
 ]
 
-# if settings.DEBUG:
-#     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)

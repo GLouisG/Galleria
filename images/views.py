bfb12b0589e815b2da3042cacc6d251b27ed4a27
GLouisG <louis.gacho@student.moringaschool.com>
2021-10-09 20:23:11 +0300

    add search results view function

--- images/views.py
+++ images/views.py
@@ -6,4 +6,13 @@ def landing(request):
     areas = Location.get_all()
     categs = Category.get_all()
     return render(request, 'index.html', {"pics": pics, "areas":areas, "categs":categs})
-    
+def search_results(request):
+    if 'image' in request.GET and request.GET["image"]:
+        search_term = request.GET.get("image")
+        searched_imgs = Image.img_searcher(search_term)
+        message = f"For {search_term}"
+
+        return render(request, 'search.html', {"term":term, "imgs":searched_imgs})
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'search.html',{"message":message}) 

9b9551a5d11cae714cbfb8366a435af336e4d95d
GLouisG <louis.gacho@student.moringaschool.com>
2021-10-10 13:29:54 +0300

    add code to admin.py

--- images/admin.py
+++ images/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from images.models import Category, Image, Location
+
+class ImageAdmin(admin.ModelAdmin):
+    filter_horizontal =('Category',)
+    filter_horizontal =('Location',)
 # Register your models here.
+admin.site.register(Category)
+admin.site.register(Location)
+admin.site.register(Image, ImageAdmin)
